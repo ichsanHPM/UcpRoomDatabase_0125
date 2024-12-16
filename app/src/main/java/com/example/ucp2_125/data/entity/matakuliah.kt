b4e75e0561462437fb5b398c4d961495ae82cf1f
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 17:26:39 +0700

    Membuat data class matakuliah

---
+++ app/src/main/java/com/example/ucp2_125/data/entity/matakuliah.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2_125.data.entity
+
+data class matakuliah(
+    val Kode : String,
+    val Nama : String,
+    val SKS : String,
+    val Semester : String,
+    val Jenis : String,
+    val DosenPengampu : String
+
+)
