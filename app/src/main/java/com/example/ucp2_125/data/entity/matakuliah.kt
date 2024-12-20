db68ced8046494222778241fc0ac23ca83f553b7
ichsanHPM <ichsanhpm@gmail.com>
2024-12-21 01:24:11 +0700

    mendefinisikan database dengan tabel matakuliah dan dosen pada KrsDatabase

--- app/src/main/java/com/example/ucp2_125/data/entity/matakuliah.kt
+++ app/src/main/java/com/example/ucp2_125/data/entity/matakuliah.kt
@@ -1,6 +1,9 @@
 package com.example.ucp2_125.data.entity
 
+import androidx.room.PrimaryKey
+
 data class matakuliah(
+    @PrimaryKey
     val Kode : String,
     val Nama : String,
     val SKS : String,
