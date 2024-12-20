f676562e6477cb62962754fd6ddc357f7873bd97
ichsanHPM <ichsanhpm@gmail.com>
2024-12-21 01:58:29 +0700

    membuat read pada dosendao

---
+++ app/src/main/java/com/example/ucp2_125/data/dao/dosendao.kt
@@ -0,0 +1,17 @@
+package com.example.ucp2_125.data.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.ucp2_125.data.entity.dosen
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface dosendao {
+    //mengambil semua data dosen  dari tabel dosen  diurutkan secara ASC
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAlldosen() : Flow<List<dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getdosen(nidn: String) : Flow<dosen>
+
+}
