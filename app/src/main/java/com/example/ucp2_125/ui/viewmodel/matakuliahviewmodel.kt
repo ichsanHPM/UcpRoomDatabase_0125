254d4e3180ecad63e0c1b2ffde99fed5d4d2d171
ichsanHPM <ichsanhpm@gmail.com>
2024-12-21 21:47:26 +0700

    membuat data class matakuliahevent

---
+++ app/src/main/java/com/example/ucp2_125/ui/viewmodel/matakuliahviewmodel.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2_125.ui.viewmodel
+
+data class matakuliahEvent(
+    val Kode: String = "",
+    val Nama: String = "",
+    val SKS: String = "",
+    val Semester: String = "",
+    val Jenis: String = "",
+    val DosenPengampu: String = ""
+
+)
