7112b0d07ef001e3101c5490ef2289c8ab96f69a
ichsanHPM <ichsanhpm@gmail.com>
2024-12-21 21:54:07 +0700

    membuat matakuliahErrorState

--- app/src/main/java/com/example/ucp2_125/ui/viewmodel/matakuliahviewmodel.kt
+++ app/src/main/java/com/example/ucp2_125/ui/viewmodel/matakuliahviewmodel.kt
@@ -1,5 +1,25 @@
 package com.example.ucp2_125.ui.viewmodel
 
+import com.example.ucp2_125.data.entity.matakuliah
+
+data class matakuliahErrorState(
+    val Kode: String? = null,
+    val Nama: String? = null,
+    val SKS: String? = null,
+    val Semester: String? = null,
+    val Jenis: String? = null,
+    val DosenPengampu: String? = null
+)
+
+fun matakuliahEvent.tomatakuliahEntity(): matakuliah = matakuliah(
+    Kode = Kode,
+    Nama = Nama,
+    SKS = SKS,
+    Semester = Semester,
+    Jenis = Jenis,
+    DosenPengampu = DosenPengampu
+)
+
 data class matakuliahEvent(
     val Kode: String = "",
     val Nama: String = "",
